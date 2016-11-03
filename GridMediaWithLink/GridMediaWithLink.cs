b4d70e394115c82e36bc2988e99cc62bd70c14dd
Thomas Kaagaard <thomas@lait.dk>
2016-11-03 11:21:55 +0100

    Add files via upload

---
+++ GridMediaWithLink/GridMediaWithLink.cs
@@ -0,0 +1,57 @@
+﻿using %SolutionName%.App.Mappers;
+using %SolutionName%.App.Models.Helpers;
+using System;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace %SolutionName%.App.App_Plugins.GridMediaWithLink
+{
+	public class GridMediaWithLinkModel
+    {
+        public int? ImageId { get; set; }
+
+        private UmbracoImage image = null;
+        public UmbracoImage Image
+        {
+            get
+            {
+                if (image == null && ImageId != null)
+                {
+                    image = UmbracoImageMapper.Map<UmbracoImage>(Convert.ToInt32(ImageId));
+                }
+                return image;
+            }
+        }
+        public string ImageTitle { get; set; }
+        public int? InternalContentId { get; set; }
+        public string UrlMode { get; set; }
+        public bool IsExternal
+        {
+            get { return UrlMode == null || UrlMode.ToLower() != "internal"; }
+        }
+        public string ExternalUrl { private get; set; }
+
+        private string url;
+        public string Url
+        {
+            get
+            {
+                if (UrlMode == "internal")
+                {
+                    if (InternalContentId != null && InternalContentId != default(int))
+                    {
+                        UmbracoHelper UH = new UmbracoHelper(UmbracoContext.Current);
+                        IPublishedContent ipc = UH.TypedContent(InternalContentId);
+                        if (ipc != null && ipc.Id > 0)
+                            url = ipc.Url;
+                    }
+                }
+                else if (UrlMode == "external")
+                {
+                    url = ExternalUrl;
+                }
+                return url;
+            }
+        }
+    }
+}
---
+++ GridMediaWithLink/editor.controller.js
@@ -0,0 +1,89 @@
+﻿angular.module("umbraco")
+    .controller("%SolutionName%.GridMediaWithLinkController", function ($scope, assetsService, dialogService, entityResource) {
+        assetsService.loadCss("/App_Plugins/GridMediaWithLink/editor.min.css");
+        // Fetch internal node names if any nodes are selected.
+        console.log($scope.control);
+
+        if ($scope.control.value && $scope.control.value.length) {
+            angular.forEach($scope.control.value,
+                function(item, key) {
+                    if (item.internalContentId) {
+                        entityResource.getById(item.internalContentId, "Document")
+                            .then(function(ent) {
+                                item.internalContentName = ent.name;
+                            });
+                    }
+                });
+        } else {
+            $scope.control.value = [{}];
+        }
+
+        $scope.openContentPicker = function (item, contentId) {
+            dialogService.contentPicker({
+                multipicker: false,
+                callback: function (data) {
+                    item.internalContentId = data.id;
+                    item.internalContentName = data.name;
+                }
+            });
+        };
+
+        $scope.addItem = function () {
+            if (!$scope.control.value)
+                $scope.control.value = [];
+            $scope.control.value.push({});
+        }
+
+        $scope.removeItem = function (index) {
+            $scope.control.value.splice(index, 1);
+        }
+
+        $scope.clearLink = function (item) {
+            item.internalContentId = null;
+            item.internalContentName = null;
+        }
+
+        $scope.openMediaPicker = function (item) {
+            dialogService.mediaPicker({
+                callback: function (data) {
+                    item.ImageId = data.id;
+                    item.ImageThumb = data.thumbnail;
+                }
+            });
+        }
+
+        $scope.removePicture = function (item) {
+            item.ImageId = null;
+            item.ImageThumb = undefined;
+        }
+
+        if ($scope.control.ImageId > 0) {
+            entityResource.getById($scope.person.ImageId, "Media").then(function (item) {
+                $scope.node = item;
+            });
+        }
+
+        $scope.openMenuMediaPicker = function (item) {
+            dialogService.mediaPicker({
+                callback: function (data) {
+                    item.MenuImageId = data.id;
+                    item.MenuImageThumb = data.thumbnail;
+                }
+            });
+        }
+
+        $scope.removeMenuPicture = function (item) {
+            item.MenuImageId = null;
+            item.MenuImageThumb = undefined;
+        }
+
+        //Internal/external url
+        $scope.internalUrl = function (item) {
+            item.UrlMode = "internal";
+        }
+
+        $scope.externalUrl = function (item) {
+            item.UrlMode = "external";
+        }
+        
+    });
